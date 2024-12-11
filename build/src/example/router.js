f2849f486feea89a5a9b63b76db1edec27b4b5d4
Lionel Wijaya <lionelchris1509@gmail.com>
2024-12-11 19:21:43 +0700

    removed build directory

--- build/src/example/router.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getExampleHandler } from "./handler";
-export var setupExampleRouter = function(router) {
-    var exampleRouter = express.Router();
-    exampleRouter.get("/:id", getExampleHandler);
-    router.use("/example", exampleRouter);
-};
--- build/src/example/validation.js
+++
@@ -1,4 +0,0 @@
-import { z } from "zod";
-export var getExampleQuerySchema = z.object({
-    id: z.string()
-});
