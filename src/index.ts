d057c65d0c7ba14f5c56cb411c202828e1f74d2b
Jayaku Briliantio <jayakubriliantio@gmail.com>
2025-01-22 21:14:14 +0700

    feat: multer upload file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
-import { errorMiddleware } from "./error";
+import { createFieldError, errorMiddleware } from "./error";
 import cookieParser from "cookie-parser";
 import { setupAuthRouter } from "./auth/router";
 import { setupUsersRouter } from "./users/router";
 import { setupSubjectsRouter } from "./subjects/router";
 import { setupClassesRouter } from "./classes/router";
+import { authMiddleware } from "./auth/middleware";
+import { uploadFile } from "./multer";
+import { MulterError } from "multer";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -18,6 +21,40 @@ setupAuthRouter(baseRouter);
 setupUsersRouter(baseRouter);
 setupClassesRouter(baseRouter);
 setupSubjectsRouter(baseRouter);
+
+baseRouter.post(
+    "/file/upload",
+    authMiddleware(["admin", "student", "superadmin", "teacher"]),
+    (req, res, next) => {
+        uploadFile(req, res, (error) => {
+            const { field } = req.body;
+
+            if (error instanceof MulterError) {
+                return next(
+                    createFieldError({
+                        [field]: "Maximum file size is 10MB.",
+                    })
+                );
+            }
+
+            if (error && error.message === "File is not supported!") {
+                return next(
+                    createFieldError({
+                        [field]: error.message,
+                    })
+                );
+            }
+
+            res.json({
+                file: req.file,
+            });
+        });
+    }
+);
+
+baseRouter.get("/file/:filename", authMiddleware(), (req, res, next) => {
+    // retrive it.
+});
 
 app.use("/api/v1", baseRouter);
 
