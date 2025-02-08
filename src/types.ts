c8491d33b407f1f7c30962a8314c5c73ae22770a
Lionel Wijaya <lionelchris1509@gmail.com>
2025-02-08 10:46:35 +0700

    feat: add BoolString type for boolean filtering

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,9 @@
 export const validStatuses = ["active", "inactive"] as const;
 export type Status = (typeof validStatuses)[number];
 
+export const validBoolStrings = ["true", "false"] as const;
+export type BoolString = (typeof validBoolStrings)[number];
+
 export type ListParams = {
     page: number;
     size: number;
