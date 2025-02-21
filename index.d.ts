a005765587617c8ca92c7fecc1f87dc9e3b99b04
Lionel Wijaya <lionelchris1509@gmail.com>
2025-02-21 07:23:19 +0700

    refactor: make user request property required

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ import { User } from "./src/users/types";
 declare global {
     namespace Express {
         interface Request {
-            user?: User;
+            user: User;
         }
     }
 }
